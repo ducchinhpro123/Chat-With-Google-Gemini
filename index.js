635142d1a4f39bd3a03eeb0d4a4e2966d3623d5a
c <c@lenovo.168.1.10.non-exists.ptr.local>
2025-04-14 16:08:47 +0700

    Update port

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 
 app.use(session({
   secret: process.env.SECRECT_KEY || 'secret',
